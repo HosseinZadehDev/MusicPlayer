7ee342ac947918ef5f8ed3f42c765dfbc5124270
MohammadMahdi HosseinZadeh <91654338+hosseinzadeh-dev@users.noreply.github.com>
2022-09-15 17:04:53 +0430

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+
+    {
+        name: "واو چه نایس",
+        artist: "اپیکور",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "پلمپ",
+        artist: "MJ سهراب",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "اووووووف",
+        artist: "مهراد هیدن - MJ سهراب",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "به مولا",
+        artist: "حصین",
+        img: "music-4",
+        src: "music-4"
+    }
+    ];
